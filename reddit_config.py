25defb3df47c9ef82f15e4dd3b7fe1099fbd45d3
Maksimys186 <maksim.kokorev86@gmail.com>
2025-05-02 21:21:12 +0500

    Update reddit_config.py

--- reddit_config.py
+++ reddit_config.py
@@ -2,8 +2,8 @@ import praw
 import os
 
 reddit = praw.Reddit(
-    client_id="eRa0jgSVayEpcfBpgdLCzw",
-    client_secret="RSxqOX9F2GCVS0n7uIZ8DmIZPMP2Pg",
+    client_id="eRa0jgSVayEpcfBpGdLCzw",
+    client_secret="RSxqOX9F2GCVSTon7uIZ8DmIZPM2Pg",
     username=os.getenv("CowEnvironmental8187"),
     password=os.getenv("vfrcvfrc1025"),
     user_agent="assorti-bot by /u/CowEnvironmental8187"
