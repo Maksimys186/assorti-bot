6166eedf0ed5cb76e111927a4f6d588c601d65fa
Maksimys186 <maksim.kokorev86@gmail.com>
2025-05-02 20:31:49 +0500

    Update and rename .env.example to reddit_config.py

---
+++ reddit_config.py
@@ -0,0 +1,9 @@
+import praw
+
+reddit = praw.Reddit(
+    client_id="eRajo9gSVayEpcfBpgdLCzw",
+    client_secret="RSxqOX9F2GCVS0n7uIZ8DmIZPMP2Pg",
+    username="CowEnvironmental8187",
+    password="ТВОЙ_ПАРОЛЬ_ОТ_REDDIT",  # Не забудь вставить свой пароль
+    user_agent="assorti-bot by /u/CowEnvironmental8187"
+)
